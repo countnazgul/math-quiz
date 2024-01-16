fb35891cb02d2c5aeda2917cafd7dfa352b86f34
Stefan Stoichev <stefan.stoichev@gmail.com>
2024-01-16 09:23:14 +0000

    1

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -9,7 +9,7 @@ import sqlite from 'better-sqlite3';
 import fs from 'fs';
 
 const db = sqlite(':memory:');
-db.exec(fs.readFileSync('schema.sql', 'utf8'));
+db.exec(fs.readFileSync('../schema.sql', 'utf8'));
 
 export const auth = lucia({
 	adapter: betterSqlite3(db, {
