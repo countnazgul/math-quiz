adafbe506d8772647e1316cb8827173b87ad7660
Stefan Stoichev <stefan.stoichev@gmail.com>
2024-01-16 09:04:22 +0000

    and again

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vitest/config";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import path from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [
+    sveltekit(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./schema.sql",
+          dest: "./",
+        },
+      ],
+    }),
+  ],
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
 });
