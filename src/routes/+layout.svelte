16e3631c61542863236925de53ba1f3ac735a051
Stefan Stoichev <sstoichev@calibrateconsulting.com>
2024-01-18 05:53:52 +0000

    show modal for the answer on small screens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
-  import {
-    colorScheme,
-    SvelteUIProvider,
-    Stack,
-    Switch,
-    Text,
-  } from "@svelteuidev/core";
+  import { colorScheme, SvelteUIProvider } from "@svelteuidev/core";
   import "../app.css";
-
-
+  import { screenWidth } from "$lib/store";
 
   colorScheme.set("dark");
 </script>
+
+<svelte:window bind:outerWidth={$screenWidth} />
 
 <svelte:head>
   <title>Math Quiz with ChatGPT</title>
